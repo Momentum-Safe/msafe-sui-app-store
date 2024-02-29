65502a0593eb19ecb3315896d82dddc6ffc94430
Aaric <aaricchen@gmail.com>
2024-02-29 16:04:13 +0800

    Fix issue

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ import { MPayAppHelper } from '@/apps/mpay/helper';
 import { CoreHelper } from '@/apps/msafe-core/helper';
 import { MSafeApps } from '@/apps/registry';
 
-export const appHelpers = new MSafeApps([new CoreHelper(), new MPayAppHelper()]);
+import { PlainTransactionHelper } from './apps/plain-transaction/helper';
+
+export const appHelpers = new MSafeApps([new CoreHelper(), new MPayAppHelper(), new PlainTransactionHelper()]);
