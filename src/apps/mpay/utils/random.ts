bfd051fef43f024bd60940290197946396f31998
Aaric <aaricchen@gmail.com>
2024-03-06 22:10:09 +0800

    Fix nanoid issue

--- src/apps/mpay/utils/random.ts
+++ src/apps/mpay/utils/random.ts
@@ -1,5 +1,12 @@
-import crypto from 'crypto';
+import { Buffer } from 'buffer';
+
+import { nanoid } from 'nanoid';
+
+if (typeof window !== 'undefined') {
+  window.global = window.global ?? window;
+  window.Buffer = window.Buffer ?? Buffer;
+}
 
 export function generateGroupId() {
-  return crypto.randomUUID();
+  return nanoid();
 }
--- src/apps/mpay/utils/randome.test.ts
+++ src/apps/mpay/utils/randome.test.ts
@@ -3,6 +3,6 @@ import { generateGroupId } from './random';
 describe('Test random', () => {
   it('UUID', () => {
     const uuid = generateGroupId();
-    expect(uuid.length).toBe(36);
+    expect(uuid.length).toBe(21);
   });
 });
