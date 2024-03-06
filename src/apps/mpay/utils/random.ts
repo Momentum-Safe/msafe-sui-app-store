eadde0381e936b13ea3b73a265adcaa6c23f940e
Aaric <aaricchen@gmail.com>
2024-03-06 23:09:52 +0800

    Fix build issue

--- src/apps/mpay/utils/random.ts
+++ src/apps/mpay/utils/random.ts
@@ -1,11 +1,4 @@
-import { Buffer } from 'buffer';
-
 import { nanoid } from 'nanoid';
-
-if (typeof window !== 'undefined') {
-  window.global = window.global ?? window;
-  window.Buffer = window.Buffer ?? Buffer;
-}
 
 export function generateGroupId() {
   return nanoid();
