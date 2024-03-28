0f906832224166ad40a7381718150e7d6f492539
CetusDevelopment <sunnykim888@proton.me>
2024-03-28 18:49:05 +0800

    fix: sui.js import

--- src/apps/cetus/intentions/farming-decrease.ts
+++ src/apps/cetus/intentions/farming-decrease.ts
@@ -1,6 +1,6 @@
 import { TransactionType } from '@msafe/sui3-utils';
 import { SuiClient } from '@mysten/sui.js/client';
-import { TransactionBlock } from '@mysten/sui.js/dist/cjs/builder';
+import { TransactionBlock } from '@mysten/sui.js/transactions';
 import { WalletAccount } from '@mysten/wallet-standard';
 
 import { CoreBaseIntention } from '@/apps/msafe-core/intention';
