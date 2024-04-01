04e3e6f7e14000da0995b357ce9f0c453bfcddf2
Aaric <aaricchen@gmail.com>
2024-04-01 11:59:10 +0800

    Remove scallop

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,14 @@ import { CoreHelper } from '@/apps/msafe-core/helper';
 import { NAVIAppHelper } from '@/apps/navi/helper';
 import { PlainTransactionHelper } from '@/apps/plain-transaction/helper';
 import { MSafeApps } from '@/apps/registry';
-import { ScallopAppHelper } from '@/apps/scallop/helper';
 import { TURBOSAppHelper } from '@/apps/turbos/helper';
 
 export const appHelpers = new MSafeApps([
   new CoreHelper(),
   new NAVIAppHelper(),
-  new TURBOSAppHelper(),
   new MPayAppHelper(),
   new PlainTransactionHelper(),
   new CetusHelper(),
   new TURBOSAppHelper(),
-  new ScallopAppHelper(),
+  //new ScallopAppHelper(),
 ]);
