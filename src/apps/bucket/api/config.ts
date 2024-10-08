0139e4a3fcfd619e249d42609ba8202925b6f230
Jacky Wang <jackyw.se@gmail.com>
2024-10-08 17:08:29 -0400

    Revert "feat: add bucket's psm actions"

--- src/apps/bucket/api/config.ts
+++
@@ -1,7 +0,0 @@
-import { SuiNetworks } from '@/types';
-import { BucketClient } from 'bucket-protocol-sdk';
-
-export const getBucketClient = (network: SuiNetworks) => {
-  const bucketClient = new BucketClient(network);
-  return bucketClient;
-};
