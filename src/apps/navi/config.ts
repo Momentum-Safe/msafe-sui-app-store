c602e5511b813619f46703c93f372c9e8395cb15
yan <yanhaibiao1991@gmail.com>
2024-10-18 14:30:17 +0800

    navi: update pools

--- src/apps/navi/config.ts
+++ src/apps/navi/config.ts
@@ -22,10 +22,10 @@ const config = {
       borrowBalanceParentId: '0xe7ff0daa9d090727210abe6a8b6c0c5cd483f3692a10610386e4dc9c57871ba7',
       supplyBalanceParentId: '0x589c83af4b035a3bc64c40d9011397b539b97ea47edf7be8f33d643606bf96f8',
     },
-    usdc: {
-      name: 'USDC',
+    wusdc: {
+      name: 'wUSDC',
       assetId: 1,
-      coinType: CoinType.usdc,
+      coinType: CoinType.wusdc,
       poolId: '0xa02a98f9c88db51c6f5efaaf2261c81f34dd56d86073387e0ef1805ca22e39c8',
       type: '0x5d4b302506645c37ff133b98c4b50a5ae14841659738d6d733d59d0d217a93bf::coin::COIN',
       reserveObjectId: '0xeb3903f7748ace73429bd52a70fff278aac1725d3b58afa781f25ce3450ac203',
@@ -115,6 +115,36 @@ const config = {
       borrowBalanceParentId: '0x551300b9441c9a3a16ca1d7972c1dbb4715e15004ccd5f001b2c2eee22fd92c1',
       supplyBalanceParentId: '0xe151af690355de8be1c0281fbd0d483c099ea51920a57c4bf8c9666fd36808fd',
     },
+    nusdc: {
+      name: 'USDC',
+      assetId: 10,
+      coinType: CoinType.nusdc,
+      poolId: '0xa3582097b4c57630046c0c49a88bfc6b202a3ec0a9db5597c31765f7563755a8',
+      type: '0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC',
+      reserveObjectId: '0x4c8a2c72a22ae8da803a8519798d312c86e74a9e0d6ec0eec2bfcf7e4b3fef5e',
+      borrowBalanceParentId: '0xb0b0c7470e96cabbb4f1e8d06bef2fbea65f4dbac52afae8635d9286b1ea9a09',
+      supplyBalanceParentId: '0x08b5ce8574ac3bc9327e66ad5decd34d07ee798f724ad01058e8855ac9acb605',
+    },
+    eth: {
+      name: 'ETH',
+      assetId: 11,
+      coinType: CoinType.eth,
+      poolId: '0x78ba01c21d8301be15690d3c30dc9f111871e38cfb0b2dd4b70cc6052fba41bb',
+      type: '0xd0e89b2af5e4910726fbcd8b8dd37bb79b29e5f83f7491bca830e94f7f226d29::eth::ETH',
+      reserveObjectId: '0x376faea6dfbffab9ea808474cb751d91222b6d664f38c0f1d23de442a8edb1ce',
+      borrowBalanceParentId: '0xf0c6ce5cfaee96073876a5fab7426043f3a798b79502c4caeb6d9772cd35af1f',
+      supplyBalanceParentId: '0xc0a0cb43620eb8a84d5a4a50a85650e7fa7ba81e660f9cc2863404fd84591d4b',
+    },
+    usdy: {
+      name: 'USDY',
+      assetId: 12,
+      coinType: CoinType.usdy,
+      poolId: '0x4b6253a9f8cf7f5d31e6d04aed4046b9e325a1681d34e0eff11a8441525d4563',
+      type: '0x960b531667636f39e85867775f52f6b1f220a058c4de786905bdf761e06a56bb::usdy::USDY',
+      reserveObjectId: '',
+      borrowBalanceParentId: '',
+      supplyBalanceParentId: '',
+    },
   } as Pool,
   borrowFee: 0.003,
   borrowFeeAddress: '0x70b9b10704263cf53392849e33b1f5fd16005869b4198ed5524836bad1234ea2',
@@ -126,7 +156,7 @@ export async function updatePackageId() {
   }
   try {
     const data = await fetch('https://open-api.naviprotocol.io/api/msafe').then((res) => res.json());
-    const { packageId, borrowFee, borrowFeeAddress } = data;
+    const { packageId, borrowFee, borrowFeeAddress, pool } = data;
     if (packageId) {
       config.ProtocolPackage = packageId;
     }
@@ -135,6 +165,9 @@ export async function updatePackageId() {
     }
     if (borrowFeeAddress) {
       config.borrowFeeAddress = borrowFeeAddress;
+    }
+    if (pool) {
+      config.pool = pool;
     }
     updated = true;
   } catch (e: any) {
