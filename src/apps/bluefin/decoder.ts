049da2ef9cbb501ff3d63d25839ff36d3aa602aa
YameenMalik <yameen@seed.im>
2024-12-13 16:01:17 -0500

    removed disable prettier

--- src/apps/bluefin/decoder.ts
+++ src/apps/bluefin/decoder.ts
@@ -1,90 +1,80 @@
-/* eslint-disable prettier/prettier */
-import { asIntN } from "@cetusprotocol/cetus-sui-clmm-sdk";
-import { TransactionType } from "@msafe/sui3-utils";
-import { fromB64 } from "@mysten/bcs";
-import { bcs } from "@mysten/sui/bcs";
-import { Transaction } from "@mysten/sui/transactions";
+import { asIntN } from '@cetusprotocol/cetus-sui-clmm-sdk';
+import { TransactionType } from '@msafe/sui3-utils';
+import { fromB64 } from '@mysten/bcs';
+import { bcs } from '@mysten/sui/bcs';
+import { Transaction } from '@mysten/sui/transactions';
 
-import { DecodeResult, TransactionSubType } from "./types";
-
-
-
+import { DecodeResult, TransactionSubType } from './types';
 
 export class Decoder {
   constructor(public readonly transaction: Transaction) {}
 
   decode() {
-    
     console.log(JSON.stringify(this.commands));
     console.log(JSON.stringify(this.inputs));
 
     if (this.isOpenPositionTx()) {
-        return this.decodeOpenPositionAndAddLiquidityTx();
-    } 
-        throw new Error(`Unknown transaction type`);
-
+      return this.decodeOpenPositionAndAddLiquidityTx();
     }
+    throw new Error(`Unknown transaction type`);
+  }
 
-    private decodeOpenPositionAndAddLiquidityTx(): DecodeResult {
+  private decodeOpenPositionAndAddLiquidityTx(): DecodeResult {
+    const openPosCommand = this.getMoveCallCommand('open_position');
+    const addLiqCommand = this.getMoveCallCommand('provide_liquidity_with_fixed_amount');
 
-        const openPosCommand = this.getMoveCallCommand("open_position");
-        const addLiqCommand = this.getMoveCallCommand("provide_liquidity_with_fixed_amount");
+    return {
+      txType: TransactionType.Other,
+      type: TransactionSubType.OpenAndAddLiquidity,
+      intentionData: {
+        pool: this.getObjectID(this.getInputIndex(openPosCommand, 1)),
+        lowerTick: Number(asIntN(BigInt(this.getU32(this.getInputIndex(openPosCommand, 2)))).toString()),
+        upperTick: Number(asIntN(BigInt(this.getU32(this.getInputIndex(openPosCommand, 3)))).toString()),
+        tokenAmount: this.getU64(this.getInputIndex(addLiqCommand, 6)),
+        maxAmountTokenA: this.getU64(this.getInputIndex(addLiqCommand, 7)),
+        maxAmountTokenB: this.getU64(this.getInputIndex(addLiqCommand, 8)),
+        isTokenAFixed: this.getBoolean(this.getInputIndex(addLiqCommand, 9)),
+      },
+    };
+  }
 
-        return {
-            txType: TransactionType.Other,
-            type: TransactionSubType.OpenAndAddLiquidity,
-            intentionData: {
-                pool: this.getObjectID(this.getInputIndex(openPosCommand, 1)),
-                lowerTick: Number(asIntN(BigInt(this.getU32(this.getInputIndex(openPosCommand, 2)))).toString()),
-                upperTick: Number(asIntN(BigInt(this.getU32(this.getInputIndex(openPosCommand, 3)))).toString()),
-                tokenAmount: this.getU64(this.getInputIndex(addLiqCommand, 6)),
-                maxAmountTokenA: this.getU64(this.getInputIndex(addLiqCommand, 7)),
-                maxAmountTokenB: this.getU64(this.getInputIndex(addLiqCommand, 8)),
-                isTokenAFixed: this.getBoolean(this.getInputIndex(addLiqCommand, 9)),
-            },
-        };
-    }
+  private get commands() {
+    return this.transaction.getData().commands;
+  }
 
+  private get inputs() {
+    return this.transaction.getData().inputs;
+  }
 
-    private get commands() {
-        return this.transaction
-          .getData()
-          .commands;
-    }
+  private isOpenPositionTx() {
+    return !!this.getMoveCallCommand('open_position');
+  }
 
-    private get inputs() {
-        return this.transaction.getData().inputs;
-    }
+  private getMoveCallCommand(fn: string) {
+    return this.commands.find((command) => command.$kind === 'MoveCall' && command.MoveCall.function === fn);
+  }
 
-    private isOpenPositionTx() {
-        return !!this.getMoveCallCommand('open_position');
-    }
+  private getObjectID(index: number): string {
+    return this.inputs[index].UnresolvedObject.objectId as string;
+  }
 
-    private getMoveCallCommand(fn: string) {
-        return this.commands.find((command) => command.$kind === 'MoveCall' && command.MoveCall.function === fn);
-    }
+  private getU32(index: number): string {
+    return String(bcs.u32().parse(Uint8Array.from(fromB64(this.inputs[index].Pure.bytes))));
+  }
 
-    private getObjectID(index: number): string {
-        return this.inputs[index].UnresolvedObject.objectId as string;
-    }
+  private getU64(index: number): string {
+    return bcs.u64().parse(Uint8Array.from(fromB64(this.inputs[index].Pure.bytes)));
+  }
 
-    private getU32(index: number): string {
-        return String(bcs.u32().parse(Uint8Array.from(fromB64(this.inputs[index].Pure.bytes))));
-    }
+  private getU128(index: number): string {
+    return bcs.u128().parse(Uint8Array.from(fromB64(this.inputs[index].Pure.bytes)));
+  }
 
-    private getU64(index: number): string {
-        return bcs.u64().parse(Uint8Array.from(fromB64(this.inputs[index].Pure.bytes)));
-    }
+  private getBoolean(index: number): boolean {
+    return bcs.bool().parse(Uint8Array.from(fromB64(this.inputs[index].Pure.bytes)));
+  }
 
-    private getU128(index: number): string {
-        return bcs.u128().parse(Uint8Array.from(fromB64(this.inputs[index].Pure.bytes)));
-    } 
-    
-    private getBoolean(index: number): boolean {
-        return bcs.bool().parse(Uint8Array.from(fromB64(this.inputs[index].Pure.bytes)));
-    }   
-
-    private getInputIndex(command: any, index: number): number {
-        return command.MoveCall.arguments[index].Input
-    }
+  private getInputIndex(command: any, index: number): number {
+    return command.MoveCall.arguments[index].Input;
+  }
 }
