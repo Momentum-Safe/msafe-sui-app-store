e9f7815f65dadd27b8d0b737af9397e3d626fef2
felix11 <khushkothari06@gmail.com>
2024-12-25 03:34:10 +0530

    stSui integration

---
+++ src/apps/stsui/decoder.ts
@@ -0,0 +1,124 @@
+import { TransactionType } from '@msafe/sui3-utils';
+import { Transaction } from '@mysten/sui/transactions';
+import { DevInspectResults } from '@mysten/sui/client';
+import { TransactionSubType } from './types';
+import { StSuiIntentionData } from './helper';
+import { MintIntentionData } from './intentions/mint';
+import { RedeemIntentionData } from './intentions/redeem';
+
+type DecodeResult = {
+  txType: TransactionType;
+  type: TransactionSubType;
+  intentionData: StSuiIntentionData;
+};
+
+type MintEventType = {
+  event: {
+    sui_amount_in: string;
+  };
+};
+
+type RedeemEventType = {
+  event: {
+    lst_amount_in: string;
+  };
+};
+
+export class Decoder {
+  constructor(
+    public readonly transaction: Transaction,
+    public readonly simResult: DevInspectResults,
+  ) {}
+
+  /**
+   * Entry point for decoding the transaction. Determines the type of transaction
+   * and calls the appropriate decode method.
+   */
+  decode(): DecodeResult {
+    if (this.isMintTransaction()) {
+      const liquidityChangeEvent = this.getLiquidityChangeEvent('MintEvent');
+      if (!liquidityChangeEvent) {
+        throw new Error('No liquidity change event found. Unable to decode transaction.');
+      }
+
+      const eventData = liquidityChangeEvent.parsedJson as MintEventType;
+      console.log('Decoder event data - ', eventData);
+
+      return this.decodeMint(eventData);
+    }
+
+    if (this.isRedeemTransaction()) {
+      const liquidityChangeEvent = this.getLiquidityChangeEvent('RedeemEvent');
+      if (!liquidityChangeEvent) {
+        throw new Error('No liquidity change event found. Unable to decode transaction.');
+      }
+
+      const eventData = liquidityChangeEvent.parsedJson as RedeemEventType;
+      console.log('Decoder event data - ', eventData);
+
+      return this.decodeRedeem(eventData);
+    }
+
+    throw new Error(`Unknown transaction type`);
+  }
+
+  // ---- Private Helpers ----
+
+  private get commands() {
+    return this.transaction.getData().commands;
+  }
+
+  private getMoveCallCommand(fn: string) {
+    return this.commands.find((command) => command.$kind === 'MoveCall' && command.MoveCall.function === fn);
+  }
+
+  private getLiquidityChangeEvent(arg: string) {
+    return this.simResult.events.find((event) => this.isLiquidityChangeEventType(event.type, arg));
+  }
+
+  //is*
+
+  private isLiquidityChangeEventType(eventType: string, arg: string) {
+    return eventType.includes(arg);
+  }
+
+  private isMintTransaction() {
+    return !!this.getMoveCallCommand('mint');
+  }
+
+  private isRedeemTransaction() {
+    return !!this.getMoveCallCommand('redeem');
+  }
+
+  // ---- Decode Methods ----
+
+  private decodeMint(eventData: MintEventType): DecodeResult {
+    const {
+      event: { sui_amount_in: amount },
+    } = eventData;
+    console.log('Decoder.decodeMint', amount);
+
+    return {
+      txType: TransactionType.Other,
+      type: TransactionSubType.MINT,
+      intentionData: {
+        amount,
+      } as MintIntentionData,
+    };
+  }
+
+  private decodeRedeem(eventData: RedeemEventType): DecodeResult {
+    const {
+      event: { lst_amount_in: amount },
+    } = eventData;
+    console.log('Decoder.decodeRedeem', amount);
+
+    return {
+      txType: TransactionType.Other,
+      type: TransactionSubType.REDEEM,
+      intentionData: {
+        amount,
+      } as RedeemIntentionData,
+    };
+  }
+}
